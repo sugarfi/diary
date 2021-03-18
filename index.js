9805facc013f49f837ee89518a265f8469b7a444
Solomon Jackson <sugarfi@sugarfi.dev>
2021-03-18 17:50:56 +0000

    e

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SUBMIT_INTERVAL = 24 * 60 * 60000;
-const SYNC_INTERVAL = 24 * 60 * 60000 + 1 * 60000;
+const SYNC_SUB_INTERVAL = 5 * 60000;
+const cron = require('node-cron');
 
 const express = require('express');
 const fs = require('fs');
@@ -9,33 +9,42 @@ app.use(express.static('public'));
 app.use(express.urlencoded());
 
 const entries = JSON.parse(fs.readFileSync('entries.json'));
-let submissions = [];
+let submissions = JSON.parse(fs.readFileSync('submissions.json'));
 
 app.get('/entries', (req, res) => {
     res.send(JSON.stringify(entries));
 });
 
+app.get('/get/submissions', (req, res) => {
+    res.send(JSON.stringify(submissions));
+});
+
 app.post('/submit', (req, res) => {
-    console.log(req.body);
+    console.log('New entry:', req.body.entry);
     submissions.push(req.body.entry);
+    console.log('Syncing submissions');
+    fs.writeFileSync('submissions.json', JSON.stringify(submissions));
     res.redirect('/');
 });
 
-setInterval(() => {
+cron.schedule('30 20 * * *', () => {
     if (submissions.length > 0) {
         console.log('Choosing submission');
         const choice = submissions[Math.floor(Math.random() * submissions.length)];
         entries.push(choice);
         submissions = [];
+        fs.writeFileSync('submissions.json', JSON.stringify(submissions));
+        console.log('Syncing');
+        fs.writeFileSync('entries.json', JSON.stringify(entries));
     } else {
         console.log('No submissions');
     }
-}, SUBMIT_INTERVAL);
+});
 
 setInterval(() => {
-    console.log('Syncing');
-    fs.writeFileSync('entries.json', JSON.stringify(entries));
-}, SYNC_INTERVAL);
+    console.log('Syncing submissions');
+    fs.writeFileSync('submissions.json', JSON.stringify(submissions));
+}, SYNC_SUB_INTERVAL);
 
 app.listen(8080, () => {
     console.log('Server up!');
